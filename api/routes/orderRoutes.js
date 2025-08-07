7b481cb6f437b8b4fa58f007e023239b006f61f3
gerti064 <gertirexhepi06@gmail.com>
2025-08-07 18:11:49 +0200

    checkout

--- api/routes/orderRoutes.js
+++ api/routes/orderRoutes.js
@@ -1,13 +1,9 @@
-// /api/routes/orderRoutes.js
 import express from 'express';
 import { createOrder } from '../controllers/orderController.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.post('/', createOrder); // ✅ This is critical — NOT '/orders'
-=======
-router.post('/orders', createOrder);
->>>>>>> 01738f75dee92684ffadfb63e82b448eb613f677
+// This handles POST /api/orders
+router.post('/', createOrder);
 
 export default router;
